64028039b6f126422025366177aa0f53b01cad87
Christoph Rudolf <christoph.rudolf@tum.de>
2017-05-24 12:17:23 +0200

    Cosmetic fixes in Onion.java and turned OnionParser into an interface.

--- src/src/main/java/de/tum/in/net/group17/onion/Onion.java
+++ src/src/main/java/de/tum/in/net/group17/onion/Onion.java
@@ -1,8 +1,8 @@
+package de.tum.in.net.group17.onion;
+
 /**
  * Created by Marko Dorfhuber(PraMiD) on 24.05.17.
  */
-
-package de.tum.in.net.group17.onion;
 
 public class Onion {
 }
--- src/src/main/java/de/tum/in/net/group17/onion/Parser/OnionParser.java
+++ src/src/main/java/de/tum/in/net/group17/onion/Parser/OnionParser.java
@@ -3,5 +3,5 @@ package de.tum.in.net.group17.onion.Parser;
 /**
  * Created by Christoph Rudolf on 24.05.17.
  */
-public class OnionParser {
+public interface OnionParser {
 }
