20dad35a0a6ac14c80717f0d6b65081355c2835b
Marko Dorfhuber <markodorfhuber@gmx.de>
2017-05-26 01:13:26 +0200

    Added the required methods to the OnionParser interface

--- src/src/main/java/de/tum/in/net/group17/onion/parser/onion/OnionParser.java
+++ src/src/main/java/de/tum/in/net/group17/onion/parser/onion/OnionParser.java
@@ -4,6 +4,30 @@ package de.tum.in.net.group17.onion.parser.onion;
  * Created by Christoph Rudolf on 24.05.17.
  * Implementation by Marko Dorfhuber(PraMiD) on 25.05.17.
  *
+ * Providing a interface to handle Onion API messages.
  */
 public interface OnionParser {
+
+    /**
+     * Create an ONION ERROR message.
+     */
+    OnionParsedObject buildOnionErrorMsg(short requestType, int tunnelID);
+
+    /**
+     * Create a OnionTunnelIncoming message.
+     * The method shall throw an IllegalArgumentExcaption on every parsing error!
+     */
+    OnionParsedObject buildOnionTunnelIncoming(int id, byte[] sourceKey);
+
+    /**
+     * Create a OnionParseObject containing a ONION TUNNEL READY message.
+     * The method shall throw an IllegalArgumentExcaption on every parsing error!
+     */
+    OnionParsedObject buildOnionTunnelReadyMsg(int id, byte[] destinationKey);
+
+    /**
+     * Parse an incomning message to the Onion module.
+     * The method shall throw an IllegalArgumentExcaption on every parsing error!
+     */
+    OnionParsedObject parseMsg(byte[] data);
 }
